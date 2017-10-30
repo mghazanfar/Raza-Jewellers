961b5cf1bf6e1a1d1bcf673df5c25d1eadd82891
Ghazanfar Ali <ghazanfar.ali@blueeast.com>
2017-10-30 09:46:51 +0500

    modified pages looks

--- src/Bridal.js
+++ src/Bridal.js
@@ -7,7 +7,7 @@ import Slick from './SlickCarousel';
 import ItemName from './ItemName';
 import About from './img/cover.png';
 import Strips from './img/strips.png';
-import Soon from './img/SOON.png';
+import Banner from './img/RingBanner.jpg';
 import Store from './img/shop.png';
 import Founder from './img/founder.png';
 import Team from './img/team.png';
@@ -19,79 +19,42 @@ import LimitedInfiniteScroll from 'react-limited-infinite-scroll'
 
 const tileData = [
    {
-   img: Founder,
+   img: Founder,  name: 'Band Ring'
  },
    {
-   img:  Founder,
+   img: Founder, name: 'Band Ring'
   },
   {
-  img:  Founder,
+  img: Founder, name: 'Band Ring'
   },
   {
-  img:  Founder,
+  img: Founder, name: 'Band Ring'
   },
-  {
-  img:  Founder,
-  },{
-  img: Founder,
-},
-  {
-  img:  Founder,
- },
- {
- img:  Founder,
- },
- {
- img:  Founder,
- },
- {
- img:  Founder,
- },{
- img: Founder,
-},
- {
- img:  Founder,
-},
-{
-img:  Founder,
-},
-{
-img:  Founder,
-},
-{
-img:  Founder,
-},{
-img: Founder,
-},
-{
-img:  Founder,
-},
-{
-img:  Founder,
-},
-{
-img:  Founder,
-},
-{
-img:  Founder,
-},
     ];
 
  const items = tileData.map((tile, index) => {
      return (
-       <Col md="3" lg="4" className="mb-5" key={index}>
+       <Col xs="12" md="5" className="mb-5" key={index}>
          <div className="containered">
            <img src={tile.img} alt="Avatar" className="image w-100" />
            <div className="middle">
-             <div className="text">John Doe</div>
+            <img src={Logo3D} width="70px" height="110px" />
+             <div className="text">{tile.name}</div>
            </div>
          </div>
        </Col>
      )
  })
  const total= items.length;
+ const fetchMore= items;
 
-class Bridal extends Component {
+
+ const loadMoreRows = () => fetchMore({
+ 			items
+    })
+
+class Bridal extends Component{
+
 
   render() {
     return (
@@ -100,7 +63,10 @@ class Bridal extends Component {
       <div className="container-fluid">
         <Row className="justify-content-center">
           <Col xs="12" className="my-4 px-0">
-            <img src={Soon} className="w-100"/>
+            <img src={Banner} className="w-100"/>
+          </Col>
+          <Col xs="8" className="my-4 px-0">
+            <h6>RAZA JEWELLERS designer rings are the qunitessence of jewellery design. either in plain gold, or crafted with precious stone, diamond rings or ancient coins, they combine refined elegance with strong determination.</h6>
           </Col>
         </Row>
       </div>
@@ -127,12 +93,17 @@ class Bridal extends Component {
           hasMore={total === undefined || items.length < total}
           spinLoader={<div className="loader">Loading...</div>}
           mannualLoader={<Buttons>Load More</Buttons>}
-          noMore={<div className="loader">No More Items</div>}
-          loadNext={this.loadNextFunc}>
-          <Row className="justify-content-center my-5">
+          noMore={<div className="loader"></div>}
+          loadNext={loadMoreRows}
+          >
+          <Row className="justify-content-around my-5">
             {items}
           </ Row>
         </LimitedInfiniteScroll>
+
+        <Row className="justify-content-center my-5">
+          <Buttons>Load More</Buttons>
+        </ Row>
       </Container>
 
       <ItemName   color="black" bgColor="#272727" divName="Footer"/>
